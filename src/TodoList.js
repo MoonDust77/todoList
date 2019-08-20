2c24ea60ddeb96bdfdb242fe4ee664496abd4cd0
qiqi <13759525126@163.com>
2019-08-20 17:08:31 +0800

    react-redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,71 +1,56 @@
-import React, { Component, Fragment } from 'react'
-import TodoItem from './TodoItem'
+import React from 'react'
+import { connect } from 'react-redux'
 
-export default class TodoList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      inputValue: '',
-      list: []
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    // this.handleItemDelete = this.handleItemDelete.bind(this)
-  }
-  render() {
-    return (
-      <Fragment>
-        <div><input value={this.state.inputValue} onChange={this.handleInputChange} ref={(input) => {this.input = input}} /><button onClick={this.handleBtnClick}>提交</button></div>
-        <ul>
-          {this.getTodoItem()}
-        </ul>
-      </Fragment>
-    )
-  }
+const TodoList = (props) => {
+  const { inputValue, list, handleInputChange, handleBtnClick, handleItemClick } = props
 
-  getTodoItem() {
-    return this.state.list.map((item, index) => {
-      return (
-        // <li key={index} onClick={this.handleItemDelete.bind(this, index)}>{item}</li>
-        <TodoItem key={index} content={item} index={index} deleteItem={this.handleItemDelete} />
-      )
-    })
-  }
-
-  handleInputChange(e) {
-    // this.setState({
-    //   inputValue: e.target.value
-    // })
-    const value = e.target.value
-    // const value = this.input.value
-    this.setState(() => ({
-      inputValue: value
-    }))
-  }
-
-  handleBtnClick = () => {
-    // this.setState({
-    //   list: [...this.state.list, this.state.inputValue],
-    //   inputValue: ''
-    // })
-    this.setState((prevState) => ({
-      list: [...prevState.list, prevState.inputValue],
-      inputValue: ''
-    }))
-  }
-
-  handleItemDelete = (index) => {
-    // const list = [...this.state.list]
-    // list.splice(index, 1)
-
-    // this.setState({
-    //   list: list
-    // })
-
-    this.setState((prevState) => {
-      const list = [...prevState.list]
-      list.splice(index, 1)
-      return {list}
-    })
-  }
-
+  return (
+    <div>
+      <div>
+        <input value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleBtnClick}>提交</button>
+      </div>
+      <ul>
+        {
+          list.map((item, index) => {
+            return <li onClick={() => { handleItemClick(index) }} key={index}>{item}</li>
+          })
+        }
+      </ul>
+    </div>
+  )
 }
+
+const mapStateToProps = (state) => {
+  return {
+    inputValue: state.inputValue,
+    list: state.list
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputChange(e) {
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+      }
+      dispatch(action)
+    },
+    handleBtnClick() {
+      const action = {
+        type: 'add_item'
+      }
+      dispatch(action)
+    },
+    handleItemClick(index) {
+      const action = {
+        type: 'delete_item',
+        index
+      }
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-// import thunk from 'redux-thunk'
-import createSagaMiddleware from 'redux-saga'
-import todoSaga from './sagas'
+import { createStore } from 'redux'
 import reducer from './reducer'
 
-// create the saga middleware
-const sagaMiddleware = createSagaMiddleware()
-
-const composeEnhancers =
-  typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose
-
-const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware))
-
-const store = createStore(reducer, enhancer)
-
-// then run the saga
-sagaMiddleware.run(todoSaga)
+const store = createStore(reducer)
 
 export default store
